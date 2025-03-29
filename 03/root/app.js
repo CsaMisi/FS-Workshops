4ae3a89f0263a450aa8ed28ab4d54a8ac2d1de8e
buffMidget <misicsanko@gmail.com>
2025-03-30 00:02:13 +0100

    almost done, only average pay per job left and looks

--- 03/root/app.js
+++ 03/root/app.js
@@ -83,23 +83,120 @@ function calculateAverageAge() {
     }
     const totalAge = data.developers.reduce((sum, dev) => sum + dev.age, 0);
     const averageAge = totalAge / data.developers.length;
-    return `Average Age: ${averageAge.toFixed(2)}`;
+    return `Átlag életkor: ${averageAge.toFixed(2)}`;
 }
 
+function calculateAveragePayForFrontend(){
+    if (!data || !data.developers || data.developers.length === 0) {
+        return "No data available";
+    }
 
+    const frontEndPay = data.developers.filter(dev=>
+        dev.job && dev.job.includes("Frontend"))
+        .reduce((sum,dev) => sum + dev.salary, 0);
+    return `Átlag Frontendes fizetés: ${(frontEndPay/data.developers.length).toFixed(2)} HUF` 
+}
+
+function calculateMultitasker(){
+    if (!data || !data.developers || data.developers.length === 0) {
+        return "No data available";
+    }
+
+    const mostSkills = data.developers.reduce((max, dev) => {
+        const skillsCount = dev.skills.length;
+        return skillsCount > max.skillsCount ? { name: dev.name, skillsCount } : max;
+    }, { name: '', skillsCount: 0 }).name;
+
+    return `${mostSkills} ért a legtöbbmindenhez`
+}
+
+function calculateCompany(){
+    if (!data || !data.developers || data.developers.length === 0) {
+        return "No data available";
+    }
+
+    const companyMap = new Map();
+    for (let i = 0; i < data.developers.length; i++) {
+        let companyFromMail = data.developers[i].email.split('@')[1].split('.')[0];
+        if(companyMap.has(`${companyFromMail}`)){
+            companyMap.set(`${companyFromMail}`,  companyMap.get(`${companyFromMail}`)+1)
+        } else{
+            companyMap.set(`${companyFromMail}`, 1);
+        }
+    }
+
+    let mostCompanies = '';
+    let maxCount = 0;
+
+    companyMap.forEach((count, company) => {
+        if (count > maxCount) {
+            maxCount = count;
+            mostCompanies = company;
+        }
+    });
+
+    return `A legtöbb fejlesztő a ${mostCompanies} cégtől van.`;
+}
+
+function calculateMinAge(){
+    if (!data || !data.developers || data.developers.length === 0) {
+        return "No data available";
+    }
+
+    let minAge = data.developers.reduce((min, dev) => {
+        return dev.age < min.age ? {name: dev.name, salary: dev.salary} : min
+    })
+
+    return `${minAge.name} a leg fiatalabb és ${minAge.salary} HUF-ot keres`
+}
+
+function calculateDiffOldestYoungest(){
+    let minAge = data.developers.reduce((min, dev) => {
+        return dev.age < min.age ? {name: dev.name, salary: dev.salary} : min
+    }).salary
+
+    console.log(minAge)
+
+    let maxAge = data.developers.reduce((max, dev) => {
+        return dev.age > max.age ? {name: dev.name, salary: dev.salary} : max
+    }).salary
+    console.log(maxAge)
+
+    return `A külömbség ${maxAge >= minAge ? (maxAge-minAge) : (minAge-maxAge)}`
+}
+
+function averagePayPerJob(){ //TODO
+    let jobMap = new Map();
+    for(let i = 0; i < data.developers.length;i++){
+        if(jobMap.has(data.developers[i].job)){
+            jobMap.set(`${data.developers[i].job}`
+                    , jobMap.get(`${data.developers[i].job}`)+data.developers[i].salary
+            )
+        } else {
+            jobMap.set(`${data.developers[i].job}`, data.developers[i].salary);
+        }
+    }
+
+}
 
 function loadQueries(){
     if(data === null){
         console.log("Data error");
         return;
     }
-    //avg
-    console.log(data.developers["age"])
+    //avg age
     document.querySelector('#Q1').innerHTML = calculateAverageAge();
-
-    
-    
-
+    //avg frontend pay
+    document.querySelector('#Q2').innerHTML = calculateAveragePayForFrontend();
+    //most skills
+    document.querySelector('#Q3').innerHTML = calculateMultitasker();
+    //most companies
+    document.querySelector('#Q4').innerHTML = calculateCompany();
+    //youngest
+    document.querySelector('#Q5').innerHTML = calculateMinAge();
+    //diff between youngest and oldest
+    document.querySelector('#Q6').innerHTML = calculateDiffOldestYoungest();
+    document.querySelector('#Q7').innerHTML = averagePayPerJob();
 }
 
 function toggleQueryVisibility(){
@@ -113,11 +210,10 @@ function toggleQueryVisibility(){
         calcBtn.value = 'Show Queries'
     }
 }
+
 document.getElementById("calcBtn").addEventListener("click", function () {
     loadQueries();
 }, { once: true });
-
-
 
 window.addEventListener("load", Init, true); 
 async function Init() {
