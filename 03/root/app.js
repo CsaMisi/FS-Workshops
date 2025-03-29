2b38e1832b6eb51c7102cca522b189f8c8dac323
buffMidget <misicsanko@gmail.com>
2025-03-29 12:54:41 +0100

    table loads now, and made changes to the looks of just the table

--- 03/root/app.js
+++ 03/root/app.js
@@ -1,4 +1,11 @@
 var data = null;
+//copied capitalize function for all objects
+Object.defineProperty(String.prototype, 'capitalize', {
+    value: function() {
+      return this.charAt(0).toUpperCase() + this.slice(1);
+    },
+    enumerable: false
+  });
 
 async function loadJson() {
     const controller = new AbortController();
@@ -25,13 +32,27 @@ function loadTable(){
         return;
     }
     console.log (data.developers.length);
-    const dTable = document.getElementById("table-target");
-   
-    if(!dTable) return;
 
-    for(i = 0; i > data.developers.length; i++){
-        const row = dTable.insertRow(i);
+    let headerRow = '';
+    if(data.developers.length > 0){
+        const headers = Object.keys(data.developers[0]);
+        console.log("headers: ", headers)
+        headers.forEach(h=>{
+            headerRow += `<th>${h.capitalize()}</th>`;
+        });
+        document.querySelector('#table-target thead tr').innerHTML = headerRow;
     }
 
-
+    let tableBody = '';
+    console.log(data);
+    data.developers.forEach(item =>{
+        let row = '<tr>';
+        Object.values(item).forEach(v =>{
+            row += `<td>${v}</td>`;
+        })
+        row += '</tr>';
+        tableBody += row;
+    });
+    //$('#table-target tbody').html(tableBody); for some reason undifined
+    document.querySelector('#table-target tbody').innerHTML = tableBody;
 }
